107c5cfd45e25fd13a5dffac110dc4efdcd08575
psurre <patrick.surre@isen.yncrea.fr>
2022-01-24 18:40:02 +0100

    Fin de la premiere journée de cours

--- app/src/main/java/fr/isen/surre/androiderestaurant/DetailActivity.kt
+++ app/src/main/java/fr/isen/surre/androiderestaurant/DetailActivity.kt
@@ -2,13 +2,20 @@ package fr.isen.surre.androiderestaurant
 import androidx.appcompat.app.AppCompatActivity
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
 
 class DetailActivity : AppCompatActivity() {
+    // Definition des variables
+    private lateinit var linearLayoutManager: LinearLayoutManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.i("DetailActivity", "***************** DetailActivity -> created ***************** ")
         setContentView(R.layout.activity_details)
+        linearLayoutManager = LinearLayoutManager(this)
+        // recycleView.layoutManager = linearLayoutManager
 
         // Récupération de l'intent
         val message = intent.getStringExtra(KEYDETAILTXT)
@@ -17,5 +24,6 @@ class DetailActivity : AppCompatActivity() {
         val textView = findViewById<TextView>(R.id.TxtDetail).apply {
             text = message
         }
+
     }
 }
--- app/src/main/java/fr/isen/surre/androiderestaurant/MainActivity.kt
+++ app/src/main/java/fr/isen/surre/androiderestaurant/MainActivity.kt
@@ -2,43 +2,47 @@ package fr.isen.surre.androiderestaurant
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
 import android.widget.TextView
-import android.widget.Toast
 import android.content.Intent
-import org.w3c.dom.Text
+import android.util.Log
+import androidx.recyclerview.widget.LinearLayoutManager
+import fr.isen.surre.androiderestaurant.databinding.ActivityMainBinding
 
 // Constantes
 public const val KEYDETAILTXT = "key.detail.txt"
 
 class MainActivity : AppCompatActivity() {
-
+    private lateinit var bindingMainAct : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        Log.i("MainActivity", "*****************  MainActivity -> created  *********************")
+        bindingMainAct = ActivityMainBinding.inflate(layoutInflater)
+        val view = bindingMainAct.root
+        setContentView(view)
+
+        bindingMainAct.txtStarter
     }
-    fun gotoEntry (view: View){
-        val txtClic = findViewById(R.id.BtnEntry) as Button
-        val msgTxt = txtClic.text.toString()
-        val intent = Intent(this, DetailActivity::class.java).apply {
-            putExtra(KEYDETAILTXT, msgTxt)
-        }
-        startActivity(intent)
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i("MainActivity", "*****************  MainActivity -> Destroyed  *******************")
     }
-    fun gotoMain (view: View){
-        val txtClic = findViewById(R.id.BtnMain) as Button
+
+    private fun dishesDetails( dishType : String) {
+        val txtClic = findViewById(R.id.txtStarter) as TextView
         val msgTxt = txtClic.text.toString()
-        val intent = Intent(this, DetailActivity::class.java).apply {
-            putExtra(KEYDETAILTXT, msgTxt)
-        }
-        startActivity(intent)
+        val changePage = Intent(this, DetailActivity::class.java)
+        changePage.putExtra(KEYDETAILTXT, dishType)
+        startActivity(changePage)
     }
-    fun gotoDesserts (view: View){
-        val txtClic = findViewById(R.id.BtnDesserts) as Button
-        val msgTxt = txtClic.text.toString()
-        val intent = Intent(this, DetailActivity::class.java).apply {
-            putExtra(KEYDETAILTXT, msgTxt)
-        }
-        startActivity(intent)
+
+    fun starterDetails (view: View){
+        dishesDetails(getString(R.string.project_starter))
+    }
+
+    fun mainCourseDetails (view: View){
+        dishesDetails(getString(R.string.project_mainCourse))
+    }
+    fun dessertDetails (view: View){
+        dishesDetails(getString(R.string.project_dessert))
     }
 }
